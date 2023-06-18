786e09d57ba6b6745574a6a979d5e498c24116fa
zhengMa <z1399956473@gmail.com>
2023-06-18 17:34:59 +0800

    Add expo-localization and i18n to support international language.

---
+++ international/lang/translate.ts
@@ -0,0 +1,49 @@
+import { getLocales } from 'expo-localization';
+import { I18n } from 'i18n-js';
+import en from './en.json'
+import cn from './cn.json'
+
+let defaultLng: string | undefined = undefined;
+const lngs: {[key: string]: {
+    data: {[key: string]: string},
+    code: string
+}} = {
+    "中文": {
+        data: cn,
+        code: "cn"
+    },
+    "English": {
+        data: cn,
+        code: "en"
+    }
+}
+const lngNameMapping: { [key: string]: string } = {
+    "中文": "cn",
+    "English":"en"
+}
+
+const deviceLanguage: string = getLocales()[0].languageCode;
+const i18n = new I18n(Object.keys(lngs).reduce((rs, key) => {
+    const lng = lngs[key];
+    rs[lng.code] = lng.data
+    return rs
+}, {}))
+i18n.locale = deviceLanguage;
+
+
+export function translateWithKey(key: string): string {
+    if(defaultLng) {
+        return en[key]
+    }
+    
+    return i18n.t(key);
+}
+
+export function resetDefaultLng(lngName: string): void {
+    const lngCode = lngNameMapping[lngName];
+    defaultLng = lngCode;
+}
+
+export function getSupportedLngNames(): string[] {
+    return Object.keys(lngNameMapping)
+}
