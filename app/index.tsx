430cc1534a94da02ff7489230b72051950dd6c4e
zhengMa <z1399956473@gmail.com>
2023-09-05 23:35:24 +0800

    Add the God as a log tool

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,65 @@
-import React from "react";
-import { NativeBaseProvider, Box } from "native-base";
+import React, {useEffect, useState} from "react";
+import * as splashScreen from 'expo-splash-screen'
+import {getLocales} from 'expo-localization'
+import {logError} from "../god/God";
+
+splashScreen.preventAutoHideAsync();
+
+type Locale = "en" | "cn"
+export interface AppContext  {
+    locale(): Locale,
+    toJSON(): string
+}
+
+interface ResourceLoader {
+    /**
+     * load the resource
+     * @param appContext context of the app.
+     * @return if load succeeded return undefined else return the reason.
+     */
+    load(appContext: AppContext): Promise<string | undefined>
+}
+
+const resources: ResourceLoader[] = []
+type LocaleResolver = (currency: string) => Locale | undefined;
+const localeResolvers: LocaleResolver[] = [
+    (currency: string): Locale | undefined => {
+        return currency === "CNY" ? "cn" : undefined
+    },
+    (currency: string): Locale | undefined => {
+        return "en"
+    }
+];
+
+async function resolveTheLocale(appContext: AppContext): Promise<Locale> {
+    const locales = getLocales();
+    if (locales === undefined || locales.length == 0) {
+        logError("Locale/resolveTheLocale", `Unable to resolve the locale of the user. ${appContext.toJSON()}`, undefined);
+        return "cn";
+    }
+    const currencyOfTheLocale = locales[0].currencyCode;
+    for (const resolver of localeResolvers) {
+        const locale = resolver(currencyOfTheLocale)
+        if(locale !== undefined) {
+            return locale;
+        }
+    }
+}
+
 
 export default function App() {
-  return (
-    <NativeBaseProvider>
-      <Box>Hello world</Box>
-    </NativeBaseProvider>
-  );
+    const [appReady, setAppReady] = useState(false);
+    useEffect(() => {
+        async function prepare() {
+            try {
+                for (const resource of resources) {
+                    resource.load()
+                }
+            } catch (ex) {
+
+            }
+        }
+
+        prepare();
+    }, [])
 }
---
+++ god/aboutGod.md
@@ -0,0 +1,17 @@
+# What is God?
+```
+ A log tool in a short term.
+```
+# What is God for ?
+```
+To let developers of this APP in 
+a state of knowning "What is happening in this app"
+``` 
+# When should I care about God?
+### When you writing code in this project
+```
+You should log the unexpected cases to God, 
+or maybe some statistic log in order to analyse.
+You know what "God" is for and when you should care about it
+if you treat this APP as a person. 
+```
