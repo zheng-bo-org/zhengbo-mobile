535b38913666dceeeefa1b376dd63453bd1932cc
zhengMa <z1399956473@gmail.com>
2023-09-08 20:50:00 +0800

    Finished layout of signIn screen

--- app/signIn/index.tsx
+++ app/signIn/index.tsx
@@ -1,9 +1,13 @@
-import {Text, View} from "react-native";
+import {Text, View, Button} from "react-native";
+import {Link} from "expo-router";
 
 export default function RoleSelectionScreen() {
     return <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
        <Text>
            I am the role selection page.
        </Text>
+        <Link href={"signIn/signInScreen"}>Teacher</Link>
+        <Link href={"signIn/signInScreen"}>Parent</Link>
+        <Link href={"signIn/signInScreen"}>Children</Link>
     </View>
 }
---
+++ app/signIn/signInScreen.tsx
@@ -0,0 +1,9 @@
+import {Text, View} from "react-native";
+
+export default function SignInScreen() {
+    return <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+       <Text>
+           I am the signIn page...
+       </Text>
+    </View>
+}
