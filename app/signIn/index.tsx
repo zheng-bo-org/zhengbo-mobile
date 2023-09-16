c1488c6d1ba0b21a17ce8936f4cea0e156deeefd
zhengMa <z1399956473@gmail.com>
2023-09-16 21:59:18 +0800

    Fixed the stuffs based on the abstraction for the theme is changed

--- app/signIn/index.tsx
+++ app/signIn/index.tsx
@@ -5,22 +5,22 @@ import React from "react";
 
 export default function RoleSelectionScreen() {
 
-    const {state: {system: {currentTheme: {theme}, currentLngPack}}, dispatch} = useAppContext();
+    const {state: {system: {currentTheme: {theme: {color}}, currentLngPack}}, dispatch} = useAppContext();
 
-    return <View style={{backgroundColor: theme["40%"], flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Text style={{color:theme["30%"], fontWeight: 'bold', fontSize: 29}}>
+    return <View style={{backgroundColor: color["40%"], flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text style={{color:color["30%"], fontWeight: 'bold', fontSize: 29}}>
             I am the role selection page.
         </Text>
 
         <Text>
-            The current language pack apply for {<Text style={{color: theme["10%"]}}>
+            The current language pack apply for {<Text style={{color: color["10%"]}}>
             {currentLngPack.applyForTheNations}
         </Text>}
         </Text>
 
         <Text>
             The content for test international functionality is:
-            <Text style={{color: theme["10%"]}}>
+            <Text style={{color: color["10%"]}}>
                 {currentLngPack.lng["/signIn/index"].test}
             </Text>
         </Text>
@@ -29,14 +29,14 @@ export default function RoleSelectionScreen() {
                 type: "changeLngPackFor",
                 payload: '中国'
             })
-        }} color={theme["10%"]}/>
+        }} color={color["10%"]}/>
 
         <Button title={"change the theme"} onPress={() => {
             dispatch({
                 type: "changeThemeTo",
                 payload: "Pink"
             })
-        }} color={theme["10%"]}/>
+        }} color={color["10%"]}/>
 
         <Link href={"signIn/signInScreen"}>Teacher</Link>
         <Link href={"signIn/signInScreen"}>Parent</Link>
--- component/theme/theme.ts
+++ component/theme/theme.ts
@@ -1,19 +1,22 @@
 /**
- * A theme is an abstraction for colors from user perspective
- * There are only four color types exists, The difference between those four types is the percent used to a screen.
- * The presents are 40% 30% 20% 10%.
- * 40%: should not be noticed by user at all, It should make user fell something, like fells "night" or "day" or "warm"
- * 30%: should be noticed by user a little bit from 40%, it's hard to describe when to use it, but you'll know that if you're an artist.
- * 20%: Users should notice 20% a little bit from 30%,
- * 10%: should absolutely be noticed by users.
+ * A theme is an abstraction for css styles from user perspective
  *
+ *  for colors:
+ *      There are only four color types exists, The difference between those four types is the percent used to a screen.
+ *      The presents are 40% 30% 20% 10%.
+ *      40%: should not be noticed by user at all, It should make user fell something, like fells "night" or "day" or "warm"
+ *      30%: should be noticed by user a little bit from 40%, it's hard to describe when to use it, but you'll know that if you're an artist.
+ *      20%: Users should notice 20% a little bit from 30%,
+ *      10%: should absolutely be noticed by users.
  * You can find more themes on this website: https://colorhunt.co/
  */
 export type Theme = {
-    "40%": string,
-    "30%": string,
-    "20%": string,
-    "10%": string
+    color: {
+        "40%": string,
+        "30%": string,
+        "20%": string,
+        "10%": string
+    }
 }
 
 export type Themes = "White" | "Pink";
@@ -29,10 +32,12 @@ export const pinkThemeDef:ThemeDef = {
     code: "Pink",
     name: "Pink",
     theme: {
-        "40%": "rgb(244, 249, 249)",
-        "30%": "rgb(241, 209, 208)",
-        "20%": "rgb(251, 172, 204)",
-        "10%": "rgb(248, 117, 170)"
+        color: {
+            "40%": "rgb(244, 249, 249)",
+            "30%": "rgb(241, 209, 208)",
+            "20%": "rgb(251, 172, 204)",
+            "10%": "rgb(248, 117, 170)"
+        }
     }
 }
 
@@ -41,10 +46,12 @@ export const whiteThemeDef: ThemeDef = {
     code: "White",
     name: "White",
     theme: {
-        "40%": "rgb(255, 255, 255)",
-        "30%": "rgb(249, 246, 247)",
-        "20%": "rgb(255, 232, 214)",
-        "10%": "rgb(255, 151, 29)"
+        color: {
+            "40%": "rgb(255, 255, 255)",
+            "30%": "rgb(249, 246, 247)",
+            "20%": "rgb(255, 232, 214)",
+            "10%": "rgb(255, 151, 29)"
+        }
     }
 }
 
