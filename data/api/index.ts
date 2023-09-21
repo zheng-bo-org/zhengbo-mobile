67e5678d7680696521794d247e395b19d773624b
zhengMa <z1399956473@gmail.com>
2023-09-22 00:22:41 +0800

    Add comments for the API

--- data/api/index.ts
+++ data/api/index.ts
@@ -1,3 +1,5 @@
+import {SystemAPI} from "./system";
+
 //Api definition in Lisp syntax
 //An API is an abstraction to completely remove the complexity to the users of the API.
 //The users of the APIS should only care about three things.
@@ -9,8 +11,6 @@
 //Furthermore, the api types or definition should be a library dependency provided by backend developer in lisp syntax.
 //Second, The API should be able to eval to http request format in any http request library,
 //Like axios or fetch, a simple example in lisp syntax: (to-axios-request apiDef data) (to-fetch-request apiDef data)
-import {SystemAPI} from "./system";
-
 export type API<T extends {
     [K in string]: {
         req: {
@@ -27,6 +27,7 @@ export type API<T extends {
     }
 }
 
+//Add more APIS here with the syntax: API<SystemAPI & xxxAPISet & AnotherAPISet>
 export type Apis = API<SystemAPI>
 export type FlattedApis  = {
     [K in keyof Apis]: {
